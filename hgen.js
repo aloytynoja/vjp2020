d56e863b6f8d6986f7e59da8fc1a3fe711f78d6e
Aleksi Löytynoja <aloytynoja@MacBook-Pro-101.wlan>
2020-05-10 08:47:25 +0300

    static version ready

---
+++ hgen.js
@@ -0,0 +1,9 @@
+/* JS mobiilinavigaatiolle */
+function navClick() {
+    var x = document.getElementById("Links");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
