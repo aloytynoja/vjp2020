e5908757f5fd2db53ff5cc9ecff3e341af48ae63
Aleksi Löytynoja <aloytynoja@MacBook-Pro-101.wlan>
2020-05-11 08:00:53 +0300

    first version of interactive element

--- hgen.js
+++ hgen.js
@@ -6,4 +6,32 @@ function navClick() {
     } else {
       x.style.display = "block";
     }
-  }
+}
+
+/* JS kommenttikentälle */
+
+const nametag = document.createElement("h6");
+var nameelement = document.getElementById("newCommentHeader");
+const commenttag = document.createElement("p");
+var commentelement = document.getElementById("newComment");
+var namemark = document.getElementById("Namemark").value;
+var comment = document.getElementById("Comment").value;
+
+function commentClick() {
+
+  /* Lisää viesti sivustolle */
+  /* Alla oleva vielä toistaiseksi väärin, käytetty lähde: https://www.tutorialspoint.com/how-to-add-a-new-element-to-html-dom-in-javascript
+  document.getElementById("all-comments").innerHTML =
+    nametag.appendChild(namemark)
+    nameelement.append(nametage)
+    commenttag.appendChild(comment)
+    commentelement.appendChild(commenttag);
+    */
+
+  /* Tyhjennä kommenttikenttien arvot */
+  document.getElementById("Name-input").value = ""
+  document.getElementById("Email-input").value = ""
+  document.getElementById("Namemark").value = ""
+  document.getElementById("Comment").value = ""
+  
+}
